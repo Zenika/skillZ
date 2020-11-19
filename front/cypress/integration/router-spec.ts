9445a2d6eb43dd0079e033c06987eb48b9580191
Ked57 <kedketh@gmail.com>
2020-11-19 03:36:05 +0100

    Wait for render

--- front/cypress/integration/router-spec.ts
+++ front/cypress/integration/router-spec.ts
@@ -1,23 +1,24 @@
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
 
-describe("Home page", function() {
-    beforeEach(function() {
-      cy.visit("/");
-    });
-  
-    it("renders the home page", function() {
-      cy.get(".content").should("contain", "home");
-    });
+describe("Home page", function () {
+  beforeEach(function () {
+    cy.visit("/");
+    cy.wait(500);// Need a better way to do wait for render
   });
 
-describe("Profile page", function() {
-    beforeEach(function() {
-      cy.visit("/profile");
-    });
-  
-    it("renders the profile page", function() {
-      cy.get(".content").should("contain", "profile");
-    });
+  it("renders the home page", function () {
+    cy.get(".content").should("contain", "home");
   });
-  
+});
+
+describe("Profile page", function () {
+  beforeEach(function () {
+    cy.visit("/profile");
+    cy.wait(500);
+  });
+
+  it("renders the profile page", function () {
+    cy.get(".content").should("contain", "profile");
+  });
+});
