462356b2b4be46152d58af2c5635e7f6114cf5d5
Ked57 <kedketh@gmail.com>
2020-11-20 16:52:27 +0100

    fix integration tests

--- front/src/Greetings/Greetings.tsx
+++ front/src/Greetings/Greetings.tsx
@@ -4,10 +4,10 @@ import "./Greetings.css";
 
 const Greetings: React.FC = () => {
   const { appState, setAppState } = useContext(appStateContext);
-
+  const userFirstName = appState.user?.firstName;
   return (
     <div>
-      <p>Greetings {appState.user.first_name}</p>
+      <p>Greetings {userFirstName}</p>
     </div>
   );
 };
