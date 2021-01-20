d92427e0eae884910b58b253a6d9519e8195d855
Ked57 <kedketh@gmail.com>
2021-01-20 14:25:46 +0100

    run prettier again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
 module.exports = {
-    important: true,
-    // Active dark mode on class basis
-    darkMode: "class",
-    i18n: {
-      locales: ["en-US", "fr-FR"],
-      defaultLocale: "en-US"
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US", "fr-FR"],
+    defaultLocale: "en-US",
+  },
+  purge: {
+    content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
+    // These options are passed through directly to PurgeCSS
+  },
+  theme: {},
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
     },
-    purge: {
-      content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"]
-      // These options are passed through directly to PurgeCSS
-    },
-    theme: {},
-    variants: {
-      extend: {
-        backgroundColor: ["checked"],
-        borderColor: ["checked"],
-        inset: ["checked"],
-        zIndex: ["hover", "active"]
-      }
-    },
-    plugins: [],
-    future: {
-      purgeLayersByDefault: true
-    }
-  };
-  
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
