7c8be6b0d3b7f40564a8354c3b8426e6028f93bc
Ked57 <kedketh@gmail.com>
2021-02-16 15:38:02 +0100

    Trying something

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,26 @@ module.exports = {
   // Active dark mode on class basis
   darkMode: "class",
   i18n: {
-    locales: ["en-US", "fr-FR"],
+    locales: ["en-US", "fr"],
     defaultLocale: "en-US",
+    domains: [
+      {
+        domain: 'fr.staging.skillz.zenika.com',
+        defaultLocale: 'fr',
+      },
+      {
+        domain: 'en.staging.skillz.zenika.com',
+        defaultLocale: 'en',
+      },
+      {
+        domain: 'fr.skillz.zenika.com',
+        defaultLocale: 'fr',
+      },
+      {
+        domain: 'en.skillz.zenika.com',
+        defaultLocale: 'en',
+      },
+    ]
   },
   // purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
   theme: {
