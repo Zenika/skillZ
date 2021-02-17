9a7b2d48aa7a2c5f6d5f28458682b102a43d04ea
Ked57 <kedketh@gmail.com>
2021-02-17 16:45:37 +0100

    don't use domains

--- next.config.js
+++ next.config.js
@@ -2,23 +2,5 @@ module.exports = {
   i18n: {
     locales: ["en", "fr"],
     defaultLocale: "en",
-    domains: [
-      {
-        domain: "fr.staging.skillz.zenika.com",
-        defaultLocale: "fr",
-      },
-      {
-        domain: "en.staging.skillz.zenika.com",
-        defaultLocale: "en",
-      },
-      {
-        domain: "fr.skillz.zenika.com",
-        defaultLocale: "fr",
-      },
-      {
-        domain: "en.skillz.zenika.com",
-        defaultLocale: "en",
-      },
-    ],
   },
 };
