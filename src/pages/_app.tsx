8e977182659b271ccf95d2c6ba6cbabd80317464
Ked57 <kedketh@gmail.com>
2021-02-17 16:59:55 +0100

    Revert "prettier"
    This reverts commit 2be1dfbc99782cd8ecceb44f5763c1c913438e8f.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import Head from "next/head";
 import { usei18n } from "../utils/usei18n";
 import { useRouter } from "next/router";
-import { useCookies } from "react-cookie";
+import { useCookies } from 'react-cookie';
 import { i18nContext } from "../utils/i18nContext";
 import React, { useState } from "react";
 import GraphQLProvider from "../components/GraphQLProvider";
@@ -17,9 +17,9 @@ if (!BASE_URL) {
 
 const App = ({ Component, pageProps }) => {
   const { push, pathname: pathName, asPath, locale } = useRouter();
-  const [cookie, setCookie] = useCookies(["NEXT_LOCALE"]);
+  const [ cookie, setCookie ] = useCookies(['NEXT_LOCALE']);
   const changeLocale = (locale: string) => {
-    if (cookie.NEXT_LOCALE !== locale) {
+    if(cookie.NEXT_LOCALE !== locale){
       setCookie("NEXT_LOCALE", locale, { path: "/" });
     }
     push(pathName, asPath, { locale });
