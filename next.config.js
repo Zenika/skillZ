b1ef7f0da82d004add10cd9bc5b6e390dd473614
Ked57 <kedketh@gmail.com>
2021-02-18 16:22:36 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -9,23 +9,5 @@ module.exports = withPWA({
   i18n: {
     locales: ["en", "fr"],
     defaultLocale: "en",
-    domains: [
-      {
-        domain: "fr.staging.skillz.zenika.com",
-        defaultLocale: "fr",
-      },
-      {
-        domain: "en.staging.skillz.zenika.com",
-        defaultLocale: "en",
-      },
-      {
-        domain: "fr.skillz.zenika.com",
-        defaultLocale: "fr",
-      },
-      {
-        domain: "en.skillz.zenika.com",
-        defaultLocale: "en",
-      },
-    ],
   },
 });
