d7c2f10b4fd0c23b322e2ab825cf2487895d9acb
Ked57 <kedketh@gmail.com>
2021-03-08 16:59:12 +0100

    Add side panel

---
+++ src/components/PageWithNavAndPanel.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Topbar from "./Topbar";
+import Navbar from "./Navbar";
+import SidePanel from "./SidePanel";
+
+const PageWithNavAndPanel = ({ children, pathName }) => {
+  const [panelOpened, setPanelOpened] = useState(false);
+  const togglePanel = () => setPanelOpened(!panelOpened);
+  const closePanelIfOpened = () => {
+    if (panelOpened) {
+      setPanelOpened(false);
+    }
+  };
+  return (
+    <>
+      <div
+        className={`z-10 ${panelOpened ? "opacity-25" : ""}`}
+        onClick={() => closePanelIfOpened()}
+      >
+        <Topbar togglePanel={togglePanel} />
+        <>{children}</>
+        <Navbar path={pathName} />
+      </div>
+      <div
+        className={`z-20 fixed inset-y-0 right-0 h-screen ${
+          panelOpened ? "w-3/4" : "w-0"
+        } dark:bg-dark-panel duration-500`}
+      >
+        <SidePanel togglePanel={togglePanel} />
+      </div>
+    </>
+  );
+};
+
+export default PageWithNavAndPanel;
---
+++ src/components/SidePanel.tsx
@@ -0,0 +1,66 @@
+import React, { useContext, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { i18nContext } from "../utils/i18nContext";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useRouter } from "next/router";
+
+const SidePanel = ({ togglePanel }) => {
+  const {
+    isAuthenticated,
+    error,
+    user,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+  const { t, changeLocale } = useContext(i18nContext);
+  const { push } = useRouter();
+  const [opened, setOpened] = useState(true);
+
+  if (error) {
+    console.error(
+      `Something bad happened while authenticating user: ${error.message}`
+    );
+    return;
+  }
+
+  return (
+    <div className="flex flex-col justify-between h-1/3 p-6">
+      <div className="flex flex-row">
+        <img className="w-16 h-16 rounded-full" src={user?.picture || ""} />
+        <div className="flex flex-col pl-4">
+          <h1 className="font-bold text-xl">{user.name}</h1>
+          <p>Zenika {}</p>
+        </div>
+      </div>
+      <ul className="flex flex-col justify-around h-full pl-4">
+        <li>
+          <Link href="/profile">
+            <div className="flex flex-row pl-4">
+              <Image src="/icons/profile.svg" width="16" height="16" />
+              <p className="pl-4">Profile</p>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link href="/preferences">
+            <div className="flex flex-row pl-4">
+              <Image src="/icons/preferences.svg" width="18" height="18" />
+              <p className="pl-4">Preferences</p>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link href="/logout">
+            <div className="flex flex-row pl-4">
+              <Image src="/icons/logout.svg" width="18" height="18" />
+              <p className="pl-4">Logout</p>
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SidePanel;
