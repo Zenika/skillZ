465be832e4949414d7ef17960081ec551bdfd848
Ked57 <kedketh@gmail.com>
2021-03-11 17:29:06 +0100

    Some work on skill management

--- src/components/SkillPanel.tsx
+++ src/components/SkillPanel.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import Image from "next/image";
+import LevelBar from "./LevelBar";
+
 const SkillPanel = ({
   name,
   level,
@@ -10,20 +14,31 @@ const SkillPanel = ({
   certif: boolean;
 }) => {
   return (
-    <div className="flex flex-col dark:bg-dark-light px-8 py-4 mx-2 my-1 rounded-lg">
-      <div className="flex flex-row justify-between">
-        <h2 className="text-xl">{name}</h2>
-        <p>certif: {certif ? "oui" : "non"}</p>
+    <div className="flex flex-row dark:bg-dark-light px-4 py-4 mx-2 my-1 rounded-lg">
+      <div className="flex flex-col w-5/6">
+        <div className="flex flex-row justify-between">
+          <h2 className="text-xl">{name}</h2>
+          <div className="pr-14">
+            {certif ? (
+              <Image src="/icons/certifs.svg" height="30" width="30" />
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-row justify-around">
+          <div className="flex flex-col">
+            <p className="text-xs text-center my-2">Desire level</p>
+            <LevelBar color="red" level={desire} />
+          </div>
+          <div className="flex flex-col">
+            <p className="text-xs text-center my-2">Skill level</p>
+            <LevelBar color="yellow" level={level} />
+          </div>
+        </div>
       </div>
-      <div className="flex flex-row justify-around">
-        <div className="flex flex-col">
-          <p className="text-xs text-center">Desire level</p>
-          <p>{desire}</p>
-        </div>
-        <div className="flex flex-col">
-          <p className="text-xs text-center">Skill level</p>
-          <p>{level}</p>
-        </div>
+      <div className="flex w-1/6 justify-end">
+        <Image src="/icons/more.svg" width="24" height="24" />
       </div>
     </div>
   );
--- src/pages/skills/[context]/[category]/index.tsx
+++ src/pages/skills/[context]/[category]/index.tsx
@@ -1,16 +1,16 @@
 import React, { useContext } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { i18nContext } from "../../../utils/i18nContext";
+import { i18nContext } from "../../../../utils/i18nContext";
 import Link from "next/link";
-import SkillPanel from "../../../components/SkillPanel";
+import SkillPanel from "../../../../components/SkillPanel";
 
 const skills = [
   {
     name: "React",
     level: 4,
     desire: 5,
-    certif: false,
+    certif: true,
   },
   {
     name: "VueJS",
@@ -26,7 +26,7 @@ const skills = [
   },
 ];
 
-const ViewSkill = () => {
+const ListSkills = () => {
   const router = useRouter();
   const { t } = useContext(i18nContext);
   const { context, category } = router.query;
@@ -41,12 +41,16 @@ const ViewSkill = () => {
       </div>
       <div className="flex flex-col h-screen p-4">
         <div className="flex flex-row justify-around px-2 py-1">
-          <button className="gradient-red flex-grow-0 rounded-full py-4 px-8">
-            MY SKILLS
-          </button>
-          <button className="dark:bg-dark-light flex-grow-0 rounded-full py-4 px-8">
-            ADD SKILLS
-          </button>
+          <Link href={`/skills/${context}/${category}`}>
+            <button className="gradient-red flex-grow-0 rounded-full py-4 px-8">
+              MY SKILLS
+            </button>
+          </Link>
+          <Link href={`/skills/${context}/${category}/add`}>
+            <button className="dark:bg-dark-light flex-grow-0 rounded-full py-4 px-8">
+              ADD SKILL
+            </button>
+          </Link>
         </div>
         <div className="flex flex-col mt-6">
           {skills.map((item) => {
@@ -65,4 +69,4 @@ const ViewSkill = () => {
   );
 };
 
-export default ViewSkill;
+export default ListSkills;
