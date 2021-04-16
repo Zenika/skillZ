9973b97e8ae253ac77a38aed3aefa3199b9dafec
Ked57 <kedketh@gmail.com>
2021-04-16 16:50:15 +0200

    Use fixed size divs instead

--- src/components/Radar.tsx
+++ src/components/Radar.tsx
@@ -42,65 +42,63 @@ const Radar = ({
       ref={container}
     >
       <div className="w-full h-4/5">
-        <table
-          className={`table-fixed w-11/12 h-5/6 m-3 border-b-2 border-${
+        <div
+          className={`w-11/12 h-5/6 m-3 border-b-2 border-${
             x === "left" ? "r" : "l"
           }-2 border-dark-red border-dashed`}
         >
-          <tbody>
-            {oneToFive.map((i) => (
-              <tr key={`${i}`}>
-                {oneToFive.map((k) => (
-                  <td
-                    key={`${i}-${k}`}
-                    className="border border-dashed border-opacity-25 border-dark-radargrid w-1/5 h-1/5"
-                  >
-                    {(() => {
-                      const filteredCircles = data.filter(
-                        (row) =>
-                          6 - Math.floor(row.x) === i &&
-                          Math.floor(row.y) + 1 === k
-                      );
-                      if (filteredCircles.length <= 0) {
-                        return <></>;
-                      }
-                      const circle = filteredCircles.reduce(
-                        (previous, current) => ({
-                          ...previous,
-                          weight: (previous.weight || 8) + 1,
-                          names: [...previous.names, ...current.names],
-                        })
-                      );
-                      const xDetail =
-                        -5 + Math.floor(Number((circle.x % 1).toFixed(1)) * 9);
-                      const yDetail =
-                        -5 + Math.floor(Number((circle.x % 1).toFixed(1)) * 13);
-                      return (
-                        <div
-                          className={`
+          {oneToFive.map((i) => (
+            <div className="flex flex-row w-full h-1/5" key={`${i}`}>
+              {oneToFive.map((k) => (
+                <div
+                  key={`${i}-${k}`}
+                  className="flex flex-col w-1/5 h-full border border-dashed border-opacity-25 border-dark-radargrid "
+                >
+                  {(() => {
+                    const filteredCircles = data.filter(
+                      (row) =>
+                        6 - Math.floor(row.x) === i &&
+                        Math.floor(row.y) + 1 === k
+                    );
+                    if (filteredCircles.length <= 0) {
+                      return <></>;
+                    }
+                    const circle = filteredCircles.reduce(
+                      (previous, current) => ({
+                        ...previous,
+                        weight: (previous.weight || 8) + 1,
+                        names: [...previous.names, ...current.names],
+                      })
+                    );
+                    const xDetail =
+                      -4 + Math.floor(Number((circle.x % 1).toFixed(1)) * 9);
+                    const yDetail =
+                      -4 + Math.floor(Number((circle.x % 1).toFixed(1)) * 13);
+                    return (
+                      <div
+                        className={`
                           flex flex-col 
                           ${styles.circle} relative text-dark-med ${
-                            xDetail < 0 ? "-" : ""
-                          }top-${Math.abs(xDetail)} ${
-                            yDetail < 0 ? "-" : ""
-                          }left-${Math.abs(
-                            yDetail
-                          )} rounded-full text-center text-xs pt-${
-                            (circle.weight || 10) - 8
-                          } h-${circle.weight || 8} w-${
-                            circle.weight || 8
-                          } gradient-${color}`} // (-4; -4) (5, 9)
-                        >
-                          {circle.names.join(",")}
-                        </div>
-                      );
-                    })()}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                          xDetail < 0 ? "-" : ""
+                        }top-${Math.abs(xDetail)} ${
+                          yDetail < 0 ? "-" : ""
+                        }left-${Math.abs(
+                          yDetail
+                        )} rounded-full text-center text-xs pt-${
+                          (circle.weight || 10) - 8
+                        } h-${circle.weight || 8} w-${
+                          circle.weight || 8
+                        } gradient-${color}`} // (-4; -4) (5, 9)
+                      >
+                        {circle.names.join(",")}
+                      </div>
+                    );
+                  })()}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
       <div className="w-full h-1/5">Title</div>
     </div>
