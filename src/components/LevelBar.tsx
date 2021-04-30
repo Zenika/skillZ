15721625c6a468e15082c727e69d09717d2ba1a8
Ked57 <kedketh@gmail.com>
2021-04-30 17:51:39 +0200

    :sparkles: Add décimal display of skill levels

--- src/components/LevelBar.tsx
+++ src/components/LevelBar.tsx
@@ -1,31 +1,57 @@
 const LevelBar = ({ color, level }) => {
   return (
     <div className="flex flex-row justify-around">
-      <div
-        className={`gradient-${color} w-4 h-2 rounded ${
-          level >= 1 ? "" : "opacity-25"
-        }`}
-      />
-      <div
-        className={`gradient-${color} w-4 h-2 ml-1 rounded ${
-          level >= 2 ? "" : "opacity-25"
-        }`}
-      />
-      <div
-        className={`gradient-${color} w-4 h-2 ml-1 rounded ${
-          level >= 3 ? "" : "opacity-25"
-        }`}
-      />
-      <div
-        className={`gradient-${color} w-4 h-2 ml-1 rounded ${
-          level >= 4 ? "" : "opacity-25"
-        }`}
-      />
-      <div
-        className={`gradient-${color} w-4 h-2 ml-1 rounded ${
-          level >= 5 ? "" : "opacity-25"
-        }`}
-      />
+      {[1, 2, 3, 4, 5].map((i) => {
+        if (level >= i + 1) {
+          return (
+            <div
+              className={`gradient-${color} w-4 h-2 ml-1 rounded opacity-100`}
+            />
+          );
+        } else if (level > i && level <= i + 0.25) {
+          console.log("level", level);
+          return (
+            <>
+              <div
+                className={`gradient-${color} w-1 h-2 ml-1 rounded-l opacity-100`}
+              />
+              <div
+                className={`gradient-${color} w-3 h-2 rounded-r opacity-25`}
+              />
+            </>
+          );
+        } else if (level > i && level <= i + 0.5) {
+          console.log("level", level);
+          return (
+            <>
+              <div
+                className={`gradient-${color} w-2 h-2 ml-1 rounded-l opacity-100`}
+              />
+              <div
+                className={`gradient-${color} w-2 h-2 rounded-r opacity-25`}
+              />
+            </>
+          );
+        } else if (level > i && level <= i + 0.75) {
+          console.log("level", level);
+          return (
+            <>
+              <div
+                className={`gradient-${color} w-3 h-2 ml-1 rounded-l opacity-100`}
+              />
+              <div
+                className={`gradient-${color} w-1 h-2 rounded-r opacity-25`}
+              />
+            </>
+          );
+        } else {
+          return (
+            <div
+              className={`gradient-${color} w-4 h-2 ml-1 rounded opacity-25`}
+            />
+          );
+        }
+      })}
     </div>
   );
 };
