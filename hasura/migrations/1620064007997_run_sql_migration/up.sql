c762cf013e4d6ba58c0157fc9f7f4789d3841c34
Ked57 <kedketh@gmail.com>
2021-05-03 20:08:28 +0200

    add migrations

---
+++ hasura/migrations/1620064007997_run_sql_migration/up.sql
@@ -0,0 +1,21 @@
+INSERT INTO public."Category" VALUES
+(DEFAULT, '89780de3-4a4c-40c2-bcdf-b5d15a48437a', 'languages-and-frameworks', 'left', 'top', 'yellow', 1),
+(DEFAULT, '06420261-3e78-4a91-bc6a-1a52cad5d6a1', 'platforms', 'right', 'top', 'violet', 2),
+(DEFAULT, 'c3341edb-3c1f-4e3d-bf89-8e795eb13690', 'tools', 'left' ,'bot', 'blue', 3),
+(DEFAULT, '89f5e9a5-5ce6-416c-bed9-dd736546aa7f', 'technics-and-methods', 'right', 'bot', 'cyan', 4);
+
+INSERT INTO public."Topic" VALUES
+(DEFAULT, '71c8f42c-7182-444d-8133-3a3a52ae7912', 'Frontend'),
+(DEFAULT, '16198ae7-40bb-4fcd-ae5d-ce371902dce2', 'Backend'),
+(DEFAULT, '5a83e55d-22f2-428a-a5e2-effade6b6be5', 'Agilité'),
+(DEFAULT, 'b53a5cdb-6269-4965-ae5b-f42c9611664f', 'Maker'),
+(DEFAULT, '817e3dae-01da-446a-920b-a3a5a9f57bea', 'Réseau'),
+(DEFAULT, '7e8e6f07-7844-4c1f-aa91-1dda49555ee3', 'Web'),
+(DEFAULT, 'eb4edee0-9351-4fce-b04f-acbee63634b0', 'Security'),
+(DEFAULT, 'c42d872f-227e-49ce-8a97-2af8aa021fbd', 'Microservices'),
+(DEFAULT, '6be68073-0f12-42bc-9835-07f8e81ea4a3', 'Network'),
+(DEFAULT, '2117fd9e-bda8-4b0a-b1f1-6ea68bca3443', 'Ops'),
+(DEFAULT, '7ee06efb-f337-4541-b160-10e35cd5b574', 'DevOps'),
+(DEFAULT, '5451823f-965c-40e7-85db-05ff8c7a370d', 'IA'),
+(DEFAULT, 'fa4af642-a890-4966-9224-2c607807ef68', 'Data'),
+(DEFAULT, 'c80dbcaa-b6d5-42fb-b375-0bb3146fcbbe', 'Mobile');
