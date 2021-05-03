78f0d11d7d6b0def71cfda031d44d4bef1239954
Ked57 <kedketh@gmail.com>
2021-05-03 18:26:33 +0200

    :bug: Fix a bug that would redirect user to localhost:3000 after logout

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,8 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+if (!NEXT_PUBLIC_BASE_URL) {
+  throw new Error(
+    "ERROR: App couldn't start because NEXT_PUBLIC_BASE_URL isn't defined"
+  );
+}
+
 const Logout = () => {
   const { logout } = useAuth0();
-  logout();
+  logout({ returnTo: NEXT_PUBLIC_BASE_URL });
   return <></>;
 };
 
