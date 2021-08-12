cce99b2d9461df1a3a9638aabb196e53b86185bd
Eric Briand <eric.briand@gmail.com>
2021-08-12 16:14:20 +0200

    fix hasura metadata

--- hasura/migrations/1625838887605_alter_table_public_UserAchievements_drop_column_metadata/down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "public"."UserAchievements" ADD COLUMN "metadata" json;
-ALTER TABLE "public"."UserAchievements" ALTER COLUMN "metadata" DROP NOT NULL;
--- hasura/migrations/1625842555339_alter_table_public_UserAchievements_alter_column_achievementLabel/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."UserAchievements" rename column "achievementLabel" to "label";
