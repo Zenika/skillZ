70fff1c7b4509f100f30e779eac31265c4b5c430
Ked57 <kedketh@gmail.com>
2022-01-10 17:56:21 +0100

    Apply same principle to skill detail page

--- src/pages/skills/[context]/[category]/[skill]/index.tsx
+++ src/pages/skills/[context]/[category]/[skill]/index.tsx
@@ -1,79 +1,61 @@
 import { gql, useQuery } from "@apollo/client";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { useRouter } from "next/router";
-import { useContext } from "react";
 import CommonPage from "../../../../../components/CommonPage";
 import PageWithSkillList from "../../../../../components/PageWithSkillList";
 import UserSkillPanel from "../../../../../components/UserSkillPanel";
-import { GetUserSkillsAndTechnicalAppetitesQuery } from "../../../../../generated/graphql";
-import {
-  GET_USERS_SKILLS_AND_DESIRES_DETAIL_BY_AGENCY_QUERY,
-  GET_USERS_SKILLS_AND_DESIRES_DETAIL_QUERY,
-} from "../../../../../graphql/queries/skills";
-import { i18nContext } from "../../../../../utils/i18nContext";
+import { useFetchUsersForSkill } from "../../../../../utils/fetchers/useFetchUsersForSkill";
+import { useNotification } from "../../../../../utils/useNotification";
 
 const SkillPage = () => {
   const router = useRouter();
-  const { t } = useContext(i18nContext);
-  const { context, category, skill, agency } = router.query;
-  const computedAgency = agency
+  let { context, category, skill, agency } = router.query;
+  agency = agency
     ? typeof agency === "string"
       ? agency
       : agency.join("")
     : undefined;
-  const { data: userSkillAndAppetiteDetails } =
-    useQuery<GetUserSkillsAndTechnicalAppetitesQuery>(
-      computedAgency
-        ? GET_USERS_SKILLS_AND_DESIRES_DETAIL_BY_AGENCY_QUERY
-        : GET_USERS_SKILLS_AND_DESIRES_DETAIL_QUERY,
-      {
-        variables: { category, skill, agency },
-      }
-    );
-  const computedSkill = skill
+  skill = skill
     ? typeof skill === "string"
       ? skill
       : skill.join("")
-    : "";
-  const computedContext = context
+    : undefined;
+  context = context
     ? typeof context === "string"
       ? context
       : context.join("")
-    : "";
-  const fetchedSkill = userSkillAndAppetiteDetails?.Category[0]?.Skills[0];
-  const computedData = fetchedSkill?.UsersCurrentSkillsAndDesires.map(
-    (userSkillDesire) => ({
-      id: fetchedSkill.id,
-      name: fetchedSkill.name,
-      level: userSkillDesire.skillLevel,
-      desire: userSkillDesire.desireLevel,
-      certif: false,
-      user: {
-        name: userSkillDesire.User.name,
-        picture: userSkillDesire.User.picture,
-        agency: userSkillDesire.User.UserLatestAgency?.agency,
-        email: userSkillDesire.User.email,
-      },
-    })
+    : undefined;
+  category = category
+    ? typeof category === "string"
+      ? category
+      : category.join("")
+    : undefined;
+  const { data, color, loading, error } = useFetchUsersForSkill(
+    category,
+    skill,
+    agency
   );
+  if (error) {
+    useNotification(`Error: ${error.message}`, "red", 5000);
+  }
   return (
     <CommonPage
       page={skill}
       faded={false}
       context={context}
       category={category}
-      skill={computedSkill}
+      skill={skill}
     >
       <PageWithSkillList
         context={context}
         category={category}
         add={false}
         faded={false}
-        color={userSkillAndAppetiteDetails?.Category[0]?.color}
+        color={color}
       >
         <>
-          {computedData?.map((data) => (
-            <UserSkillPanel skill={data} context={computedContext} />
+          {data?.map((data) => (
+            <UserSkillPanel skill={data} context={context as string} />
           ))}
         </>
       </PageWithSkillList>
