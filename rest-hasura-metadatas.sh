b9cdbc7b221919dc5abd0647027a7d5042feb0a3
Mai-ly <mai-ly.lehoux@zenika.com>
2022-05-13 15:43:11 +0200

    delete rest script

--- rest-hasura-metadatas.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-REST_ENDPOINTS_METADATA_FILE=$1 #query_collections.yaml
-HASURA_GRAPHQL_DATABASE_URL=$2
-REST_ENDPOINTS_METADATA="$(python3 -c 'import sys, yaml, json; y=yaml.safe_load(sys.stdin.read()); print(json.dumps(y))' < "$REST_ENDPOINTS_METADATA_FILE")"
-psql -d $HASURA_GRAPHQL_DATABASE_URL -P pager -c "UPDATE hdb_catalog.hdb_metadata SET metadata = jsonb_insert(metadata::jsonb, '{rest_endpoints}', '$REST_ENDPOINTS_METADATA'::jsonb, false)"
