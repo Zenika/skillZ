cabad3c1712763c0505ad4a0c912a24bbfa67049
Benjamin Bourgeois <benjamin.bourgeois@zenika.com>
2022-09-14 11:21:19 +0200

    :rocket: add new loading page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,26 @@
-import { useContext } from "react";
-import { i18nContext } from "../utils/i18nContext";
+import Image from "next/image";
+import React from "react";
+import { useDarkMode } from "../utils/darkMode";
 
 const Loading = () => {
-  const { t } = useContext(i18nContext);
-  return <div>{t("loading.loadingText")}</div>;
+  const { darkMode } = useDarkMode();
+
+  return (
+    <section
+      className={`${
+        darkMode ? "bg-dark-dark" : "bg-light-ultrawhite"
+      } relative place-items-center grid h-screen w-screen gap-4`}
+    >
+      <div className="bg-dark-red w-48 h-48  absolute animate-ping rounded-full delay-5s shadow-xl"></div>
+      <div className="bg-dark-red w-32 h-32 absolute animate-ping rounded-full shadow-xl"></div>
+      <div className="bg-dark-red w-28 h-28 absolute animate-pulse rounded-full shadow-xl"></div>
+      <Image
+        src={`/icons/${darkMode ? "dark" : "light"}/logo.svg`}
+        height="35"
+        width="105"
+      />
+    </section>
+  );
 };
 
 export default Loading;
