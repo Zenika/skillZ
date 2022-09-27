71d4009892e2367652c4a2ca7cc35d2673f0b1e3
Mai-ly <mai-ly.lehoux@zenika.com>
2022-09-27 16:13:24 +0200

    :sparkles: add buttons to acces to new route /radar

--- src/pages/profile/[email]/index.tsx
+++ src/pages/profile/[email]/index.tsx
@@ -13,11 +13,14 @@ import { UserCertification } from "../../../utils/types";
 import Custom404 from "../../404";
 import { GetUserAgencyAndAllAgenciesQuery } from "../../../generated/graphql";
 import { INSERT_USER_MUTATION } from "../../../graphql/mutations/userInfos";
+import { config } from "../../../env";
+import { GoGraph } from "react-icons/go";
 
 const Profile = () => {
   const router = useRouter();
   const { t } = useContext(i18nContext);
   const { context, email: userEmail } = router.query;
+  const { push } = useRouter();
   const { data, error, loading } = useQuery<GetUserAgencyAndAllAgenciesQuery>(
     GET_USER_AGENCY_AND_ALL_AGENCIES_QUERY,
     {
@@ -46,22 +49,39 @@ const Profile = () => {
   const userCertifications =
     error || data?.UserCertification.length <= 0 ? [] : data?.UserCertification;
 
+  const linkRadar = new URL(
+    `${config.nextPublicBaseUrl}/profile/${infoUser?.email}/radars`
+  );
+  console.log("infoUser", infoUser);
+
   return (
     <div>
       {(data?.User?.length > 0 && !error) || loading ? (
         <CommonPage page={"profile"} faded={false} context={context}>
           <div className="flex flex-row justify-center mt-4 mb-20">
             <div className="flex flex-col justify-center max-w-screen-md w-full p-4">
-              <div className="flex flex-row justify-start">
-                <img
-                  className="w-16 h-16 rounded-full"
-                  height="64"
-                  width="64"
-                  src={infoUser?.picture || ""}
-                />
-                <div className="flex flex-col mx-4 justify-center">
-                  <span>{infoUser?.name}</span>
+              <div className="flex flex-row place-content-between mb-2">
+                <div className="flex flex-row justify-start">
+                  <img
+                    className="w-16 h-16 rounded-full"
+                    height="64"
+                    width="64"
+                    src={infoUser?.picture || ""}
+                  />
+
+                  <div className="flex flex-col mx-4 justify-center">
+                    <span>{infoUser?.name}</span>
+                  </div>
                 </div>
+                <button
+                  className="px-5 gradient-red rounded-full disabled:opacity-25"
+                  onClick={() => push(linkRadar)}
+                >
+                  <div className="grid justify-items-center">
+                    <GoGraph></GoGraph>
+                    See radars
+                  </div>
+                </button>
               </div>
               {infoUser?.UserLatestAgency?.agency ? (
                 <ViewAgency
---
+++ src/pages/profile/[email]/radars/index.tsx
@@ -0,0 +1,24 @@
+type HomePanelProps = {
+  props: {
+    x: string;
+    y: string;
+    context: string;
+    color: string;
+    name: string;
+    count: number;
+    data: {
+      x: number;
+      y: number;
+      weight: number;
+      labels: string[];
+      name: string;
+    }[];
+    certifs: number;
+  };
+};
+
+const HomePanel = ({}) => {
+  return <div></div>;
+};
+
+export default HomePanel;
