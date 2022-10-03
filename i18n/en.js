7d6f953502788f5520e6b5cfe1c937f9e918456f
Benjamin Bourgeois <benjamin.bourgeois@zenika.com>
2022-10-03 11:01:56 +0200

    :lipstick: improve ui of menu dropdown

--- i18n/en.js
+++ i18n/en.js
@@ -14,6 +14,7 @@ export default {
     search: "Search",
     darkmode: "Dark",
     classic: "Classic",
+    preferences: "Preferences",
   },
   sidepanel: {
     profile: "Profile",
--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,12 +1,13 @@
-import { useContext } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import Image from "next/image";
 import Link from "next/link";
-import { i18nContext } from "../utils/i18nContext";
-import { useAuth0 } from "@auth0/auth0-react";
-import { LocaleSelector } from "./LocaleSelector";
-import { DarkModeSelector } from "./DarkModeSelector";
-import { useDarkMode } from "../utils/darkMode";
 import { useRouter } from "next/router";
+import React, { useContext } from "react";
+import { useDarkMode } from "../utils/darkMode";
+import { i18nContext } from "../utils/i18nContext";
+import { BotNotifications } from "./BotNotifications";
+import { DarkModeSelector } from "./DarkModeSelector";
+import { LocaleSelector } from "./LocaleSelector";
 
 const SidePanel = () => {
   const { locale } = useRouter();
@@ -34,10 +35,10 @@ const SidePanel = () => {
           <h1 className="font-bold text-xl">{user.name}</h1>
         </div>
       </div>
-      <ul className="flex flex-col justify-around h-full mt-4 pl-4">
-        <li>
+      <ul className="flex flex-col justify-around p-2 mt-4">
+        <li className="p-3 hover:bg-light-med dark:hover:bg-dark-med">
           <Link href="/profile">
-            <div className="flex flex-row pl-4 cursor-pointer">
+            <div className="flex flex-row cursor-pointer">
               <Image
                 src={`/icons/${darkMode ? "dark" : "light"}/profile.svg`}
                 width="16"
@@ -47,19 +48,9 @@ const SidePanel = () => {
             </div>
           </Link>
         </li>
-        <li className="p-2">
-          <LocaleSelector locale={locale} changeLocale={changeLocale} t={t} />
-        </li>
-        <li className="p-2">
-          <DarkModeSelector
-            darkMode={darkMode}
-            changeDarkMode={changeDarkMode}
-            t={t}
-          />
-        </li>
-        <li>
+        <li className="p-3 hover:bg-light-med dark:hover:bg-dark-med">
           <Link href="/logout">
-            <div className="flex flex-row pl-4 cursor-pointer">
+            <div className="flex flex-row cursor-pointer">
               <Image
                 src={`/icons/${darkMode ? "dark" : "light"}/logout.svg`}
                 width="18"
@@ -68,6 +59,28 @@ const SidePanel = () => {
               <p className="pl-4">{t("sidepanel.logout")}</p>
             </div>
           </Link>
+        </li>
+        {/*Separator*/}
+        <div className="flex items-center py-2">
+          <div className="flex-grow h-px dark:bg-dark-radargrid bg-light-radargrid"></div>
+          <span className="flex-shrink text-xs dark:text-dark-graytext text-light-graytext px-4 italic font-light">
+            {t("nav.preferences")}
+          </span>
+          <div className="flex-grow h-px dark:bg-dark-radargrid bg-light-radargrid"></div>
+        </div>
+        {/*Seperator*/}
+        <li className="p-4">
+          <LocaleSelector locale={locale} changeLocale={changeLocale} t={t} />
+        </li>
+        <li className="p-4">
+          <DarkModeSelector
+            darkMode={darkMode}
+            changeDarkMode={changeDarkMode}
+            t={t}
+          />
+        </li>
+        <li className="p-4">
+          <BotNotifications t={t}></BotNotifications>
         </li>
       </ul>
     </div>
