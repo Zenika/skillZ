8e2d99b749f082efe7808d46b944ecb5aaf854bb
PierreBrethes <pierre.brethes@zenika.com>
2022-11-23 09:57:53 +0100

    💄 admin notifications for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,33 @@ import { useRouter } from "next/router";
 import { useContext } from "react";
 import { useDarkMode } from "../utils/darkMode";
 import { i18nContext } from "../utils/i18nContext";
+import { useMediaQuery } from "react-responsive";
 import { config } from "../env";
+import { GetAllNotVerifiedSkillsQuery } from "../generated/graphql";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_NOT_VERIFIED_SKILL } from "../graphql/queries/skills";
 
 const Navbar = () => {
   const { t } = useContext(i18nContext);
   const { darkMode } = useDarkMode();
   const { pathname } = useRouter();
   const { user } = useAuth0();
+
+  const { data: skills, error: errorSkills } =
+    useQuery<GetAllNotVerifiedSkillsQuery>(GET_ALL_NOT_VERIFIED_SKILL, {
+      fetchPolicy: "network-only",
+    });
+
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1280px)",
+  });
+
+  const numberOfNotifications =
+    !errorSkills && skills ? (
+      <div className="inline-flex justify-center items-center w-5 h-5 text-xs font-bold text-light-ultrawhite bg-dark-red rounded-full dark:border-gray-900">
+        {skills.Skill.length}
+      </div>
+    ) : null;
 
   return (
     <header className="p-2 z-50 flex flex-auto flex-row justify-evenly h-16 bg-red-800 bg-light-light dark:bg-dark-dark text-light-graytext dark:text-dark-graytext inset-x-0 bottom-0 overflow-hidden fixed shadow-2xl">
@@ -98,28 +118,29 @@ const Navbar = () => {
           .split(";")
           .find((admin) => admin === user.email) && (
         <Link href="/admin">
-          <div className="flex flex-initial flex-col justify-between cursor-pointer relative">
-            <Image
-              src={
-                pathname === "/admin"
-                  ? `/icons/${darkMode ? "dark" : "light"}/zenika-selected.svg`
-                  : `/icons/${darkMode ? "dark" : "light"}/zenika.svg`
-              }
-              alt={"admin"}
-              width="25"
-              height="25"
-              className="p-1"
-            />
-            {/* {skills && (
-                          <div
-                            className={
-                              "absolute top-0 right-5 inline-flex justify-center items-center w-5 h-5 text-xs font-bold text-light-ultrawhite bg-dark-red rounded-full dark:border-gray-900"
-                            }
-                          >
-                            {skills.Skill.length}
-                          </div>
-                        )} */}
-
+          <div className="flex flex-initial flex-col justify-between cursor-pointer relative items-center">
+            {isDesktop ? (
+              <>
+                <Image
+                  src={
+                    pathname === "/admin"
+                      ? `/icons/${
+                          darkMode ? "dark" : "light"
+                        }/zenika-selected.svg`
+                      : `/icons/${darkMode ? "dark" : "light"}/zenika.svg`
+                  }
+                  alt={"admin"}
+                  width="25"
+                  height="25"
+                  className="p-1"
+                />
+                <div className={"absolute top-0 right-5"}>
+                  {numberOfNotifications}
+                </div>
+              </>
+            ) : (
+              <>{numberOfNotifications}</>
+            )}
             <span className="text-center">Admin</span>
             {pathname === "/admin" && (
               <div className="flex flex-row justify-center w-full h-0.5">
