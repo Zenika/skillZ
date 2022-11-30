96316adcac0e65af7ad159d50ea61422714c8c8e
Mai-ly <mai-ly.lehoux@zenika.com>
2022-11-30 17:51:13 +0100

    :zap: delete unused and add new check for suggestions tags

--- src/components/AutoCompleteList.tsx
+++ src/components/AutoCompleteList.tsx
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from "react";
 
 type AutoCompleteList = {
   choices: any[];
-  placeholder: string;
   onChange: (choice: any) => void;
   search: string;
 };
@@ -28,7 +27,7 @@ const AutoCompleteList = ({
         className="z-10 absolute"
         style={{ width: size.width, height: size.height }}
       >
-        {search.length > 0 && (
+        {choices && choices.length > 0 && search.length > 0 && (
           <div className="flex w-full flex-col bg-light-light dark:bg-dark-light overflow-y-scroll max-h-96 mt-1 rounded-lg border border-solid border-light-dark">
             {choices.map((choice, i) => (
               <div
