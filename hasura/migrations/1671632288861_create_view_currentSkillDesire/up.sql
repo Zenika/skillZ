3adb43f9883a98d3b997a793c457dcd7e031ae3a
Mai-ly <mai-ly.lehoux@zenika.com>
2022-12-21 15:22:00 +0100

    :bug: resolve confilcits metadatas/migrations

---
+++ hasura/migrations/1671632288861_create_view_currentSkillDesire/up.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW "public"."UsersCurrentSkillsAndDesires" AS 
+ SELECT "Skill".id AS "skillId",
+    "Skill"."categoryId",
+    "UserSkillDesire"."userEmail",
+    "Skill".name,
+    "UserSkillDesire"."skillLevel",
+    "UserSkillDesire"."desireLevel",
+    "UserSkillDesire".created_at
+   FROM (("Skill"
+     JOIN "UserSkillDesire" ON (("Skill".id = "UserSkillDesire"."skillId")))
+     JOIN ( SELECT "UserSkillDesire_1"."userEmail",
+            "UserSkillDesire_1"."skillId",
+            max("UserSkillDesire_1".created_at) AS created_at
+           FROM "UserSkillDesire" "UserSkillDesire_1"
+          GROUP BY "UserSkillDesire_1"."userEmail", "UserSkillDesire_1"."skillId") "Source" ON ((("UserSkillDesire"."skillId" = "Source"."skillId") AND ("UserSkillDesire"."userEmail" = "Source"."userEmail") AND ("UserSkillDesire".created_at = "Source".created_at))))
+  ORDER BY "UserSkillDesire".created_at DESC;
