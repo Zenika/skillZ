e2e0c0bbc64e2631277df18171c52d9f38039a9f
Benjamin Bourgeois <benjishift@gmail.com>
2022-12-21 15:57:50 +0100

    :construction: fix endpoint

---
+++ src/utils/fetchers/getAllSkillsFetcher.tsx
@@ -0,0 +1,24 @@
+import { of } from "await-of";
+import { fetcher } from "../fetcher";
+
+export const GetAllSkillsQuery = `
+query getAllSkills {
+  Skill(where: {verified: {_eq: true}}, order_by: {name: asc}) {
+    id
+    categoryId
+    description
+    name
+    verified
+  }
+}`;
+
+export const GetAllSkillsFetcher = async () => {
+  const [response, err] = await of(fetcher(GetAllSkillsQuery, {}));
+
+  if (err) {
+    console.error(err);
+    return null;
+  }
+
+  return await response.json();
+};
