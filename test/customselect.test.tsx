994213992c4592aa1260c4b287b5c3f3fe4b700e
Benjamin Bourgeois <benjishift@gmail.com>
2023-01-23 09:56:00 +0100

    :arrow_up: fix tests after upgrade

--- test/customselect.test.tsx
+++ test/customselect.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import CustomSelect, {
   customSelectClasses,
 } from "../src/components/atoms/CustomSelect/CustomSelect";
@@ -171,7 +171,9 @@ describe("CustomSelect component", () => {
     expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
 
     // Click on the placeholder
-    fireEvent.click(customSelect.closest("div"));
+    act(() => {
+      fireEvent.click(customSelect.closest("div"));
+    });
 
     // Expect the dropdown to be open
     expect(screen.getByText("Choice 1")).toBeInTheDocument();
@@ -187,227 +189,227 @@ describe("CustomSelect component", () => {
     );
 
     // Close the dropdown
-    customSelect.closest("div").click();
+    act(() => customSelect.closest("div").click());
 
     // Expect the dropdown to be closed
     expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
   });
-  //
-  // it("renders a CustomSelect with complex choices, open and close", () => {
-  //   const keyFn = jest.fn((item) => item.id);
-  //   const labelFn = jest.fn((item) => item.name);
-  //   let value = null;
-  //   const onChange = jest.fn((val) => {
-  //     value = val;
-  //   });
-  //   const choices = [
-  //     { id: 1, name: "Choice 1" },
-  //     { id: 2, name: "Choice 2" },
-  //     { id: 3, name: "Choice 3" },
-  //     { id: 4, name: "Choice 4" },
-  //   ];
-  //
-  //   const { rerender } = render(
-  //     <DarkModeProvider
-  //       value={{ darkMode: false, changeDarkMode: jest.fn(() => false) }}
-  //     >
-  //       <CustomSelect
-  //         choices={choices}
-  //         keyFn={keyFn}
-  //         labelFn={labelFn}
-  //         placeholder="This is a placeholder"
-  //         onChange={onChange}
-  //       ></CustomSelect>
-  //     </DarkModeProvider>
-  //   );
-  //
-  //   const customSelect = screen.getByText("This is a placeholder");
-  //
-  //   expect(customSelect).toBeInTheDocument();
-  //
-  //   // Expect the dropdown to be closed
-  //   expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
-  //
-  //   // Click on the placeholder
-  //   customSelect.closest("div").click();
-  //
-  //   // Expect the dropdown to be open
-  //   expect(screen.getByText("Choice 1")).toBeInTheDocument();
-  //   expect(screen.getByText("Choice 2")).toBeInTheDocument();
-  //   expect(screen.getByText("Choice 3")).toBeInTheDocument();
-  //   expect(screen.getByText("Choice 4")).toBeInTheDocument();
-  //
-  //   // Close the dropdown
-  //   customSelect.closest("div").click();
-  //
-  //   // Expect the dropdown to be closed
-  //   expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
-  // });
-  //
-  // it("renders a CustomSelect with simple choices, open and select a choice", () => {
-  //   const keyFn = jest.fn((item) => item);
-  //   const labelFn = jest.fn((item) => item);
-  //   let value = null;
-  //   const onChange = jest.fn((val) => {
-  //     value = val;
-  //   });
-  //   const choices = ["Choice 1", "Choice 2", "Choice 3", "Choice 4"];
-  //
-  //   const { rerender } = render(
-  //     <DarkModeProvider
-  //       value={{ darkMode: false, changeDarkMode: jest.fn(() => false) }}
-  //     >
-  //       <CustomSelect
-  //         choices={choices}
-  //         keyFn={keyFn}
-  //         labelFn={labelFn}
-  //         placeholder="This is a placeholder"
-  //         onChange={onChange}
-  //       ></CustomSelect>
-  //     </DarkModeProvider>
-  //   );
-  //
-  //   const customSelect = screen.getByText("This is a placeholder");
-  //
-  //   expect(customSelect).toBeInTheDocument();
-  //
-  //   // Expect the dropdown to be closed
-  //   expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
-  //
-  //   // Click on the placeholder
-  //   customSelect.closest("div").click();
-  //
-  //   // Expect the dropdown to be open
-  //   expect(screen.getByText("Choice 1")).toBeInTheDocument();
-  //
-  //   // Click on a choice
-  //   screen.getByText("Choice 1").click();
-  //
-  //   // Expect the dropdown to be closed
-  //   expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
-  //
-  //   // Expect the placeholder to be updated
-  //   expect(screen.getByText("Choice 1")).toBeInTheDocument();
-  //
-  //   // Expect the value to be updated
-  //   expect(value).toEqual("Choice 1");
-  //
-  //   // Expect the onChange to be called
-  //   expect(onChange).toHaveBeenCalledTimes(1);
-  //
-  //   // Expect the onChange to be called with the correct value
-  //   expect(onChange).toHaveBeenCalledWith("Choice 1");
-  //
-  //   // Expect the keyFn to be called
-  //   expect(keyFn).toHaveBeenCalledTimes(4);
-  //
-  //   // Expect the keyFn to be called with the correct value
-  //   expect(keyFn).toHaveBeenCalledWith("Choice 1");
-  //   expect(keyFn).toHaveBeenCalledWith("Choice 2");
-  //   expect(keyFn).toHaveBeenCalledWith("Choice 3");
-  //   expect(keyFn).toHaveBeenCalledWith("Choice 4");
-  //
-  //   // Expect the labelFn to be called
-  //   expect(labelFn).toHaveBeenCalledTimes(5);
-  //
-  //   // Expect the labelFn to be called with the correct value
-  //   expect(labelFn).toHaveBeenCalledWith("Choice 1");
-  //   expect(labelFn).toHaveBeenCalledWith("Choice 2");
-  //   expect(labelFn).toHaveBeenCalledWith("Choice 3");
-  //   expect(labelFn).toHaveBeenCalledWith("Choice 4");
-  // });
-  //
-  // it("renders a CustomSelect with complex choices, open and select a choice", () => {
-  //   const keyFn = jest.fn((item) => item.id);
-  //   const labelFn = jest.fn((item) => item.name);
-  //   let value = null;
-  //   const onChange = jest.fn((val) => {
-  //     value = val;
-  //   });
-  //   const choices = [
-  //     { id: 1, name: "Choice 1" },
-  //     { id: 2, name: "Choice 2" },
-  //     { id: 3, name: "Choice 3" },
-  //     { id: 4, name: "Choice 4" },
-  //   ];
-  //
-  //   const { rerender } = render(
-  //     <DarkModeProvider
-  //       value={{ darkMode: false, changeDarkMode: jest.fn(() => false) }}
-  //     >
-  //       <CustomSelect
-  //         choices={choices}
-  //         keyFn={keyFn}
-  //         labelFn={labelFn}
-  //         placeholder="This is a placeholder"
-  //         onChange={onChange}
-  //       ></CustomSelect>
-  //     </DarkModeProvider>
-  //   );
-  //
-  //   const customSelect = screen.getByText("This is a placeholder");
-  //
-  //   expect(customSelect).toBeInTheDocument();
-  //
-  //   // Expect the dropdown to be closed
-  //   expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
-  //
-  //   // Click on the placeholder
-  //   customSelect.closest("div").click();
-  //
-  //   // Expect the dropdown to be open
-  //   expect(screen.getByText("Choice 1")).toBeInTheDocument();
-  //
-  //   // Click on a choice
-  //   screen.getByText("Choice 1").click();
-  //
-  //   // Expect the dropdown to be closed
-  //   expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
-  //   expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
-  //
-  //   // Expect the placeholder to be updated
-  //   expect(screen.getByText("Choice 1")).toBeInTheDocument();
-  //
-  //   // Expect the value to be updated
-  //   expect(value).toEqual({ id: 1, name: "Choice 1" });
-  //
-  //   // Expect the onChange to be called
-  //   expect(onChange).toHaveBeenCalledTimes(1);
-  //
-  //   // Expect the onChange to be called with the correct value
-  //   expect(onChange).toHaveBeenCalledWith({ id: 1, name: "Choice 1" });
-  //
-  //   // Expect the keyFn to be called
-  //   expect(keyFn).toHaveBeenCalledTimes(4);
-  //
-  //   // Expect the keyFn to be called with the correct value
-  //   expect(keyFn).toHaveBeenCalledWith({ id: 1, name: "Choice 1" });
-  //   expect(keyFn).toHaveBeenCalledWith({ id: 2, name: "Choice 2" });
-  //   expect(keyFn).toHaveBeenCalledWith({ id: 3, name: "Choice 3" });
-  //   expect(keyFn).toHaveBeenCalledWith({ id: 4, name: "Choice 4" });
-  //
-  //   // Expect the labelFn to be called
-  //   expect(labelFn).toHaveBeenCalledTimes(5);
-  //
-  //   // Expect the labelFn to be called with the correct value
-  //   expect(labelFn).toHaveBeenCalledWith({ id: 1, name: "Choice 1" });
-  //   expect(labelFn).toHaveBeenCalledWith({ id: 2, name: "Choice 2" });
-  //   expect(labelFn).toHaveBeenCalledWith({ id: 3, name: "Choice 3" });
-  //   expect(labelFn).toHaveBeenCalledWith({ id: 4, name: "Choice 4" });
-  // });
+
+  it("renders a CustomSelect with complex choices, open and close", () => {
+    const keyFn = jest.fn((item) => item.id);
+    const labelFn = jest.fn((item) => item.name);
+    let value = null;
+    const onChange = jest.fn((val) => {
+      value = val;
+    });
+    const choices = [
+      { id: 1, name: "Choice 1" },
+      { id: 2, name: "Choice 2" },
+      { id: 3, name: "Choice 3" },
+      { id: 4, name: "Choice 4" },
+    ];
+
+    const { rerender } = render(
+      <DarkModeProvider
+        value={{ darkMode: false, changeDarkMode: jest.fn(() => false) }}
+      >
+        <CustomSelect
+          choices={choices}
+          keyFn={keyFn}
+          labelFn={labelFn}
+          placeholder="This is a placeholder"
+          onChange={onChange}
+        ></CustomSelect>
+      </DarkModeProvider>
+    );
+
+    const customSelect = screen.getByText("This is a placeholder");
+
+    expect(customSelect).toBeInTheDocument();
+
+    // Expect the dropdown to be closed
+    expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
+
+    // Click on the placeholder
+    act(() => customSelect.closest("div").click());
+
+    // Expect the dropdown to be open
+    expect(screen.getByText("Choice 1")).toBeInTheDocument();
+    expect(screen.getByText("Choice 2")).toBeInTheDocument();
+    expect(screen.getByText("Choice 3")).toBeInTheDocument();
+    expect(screen.getByText("Choice 4")).toBeInTheDocument();
+
+    // Close the dropdown
+    act(() => customSelect.closest("div").click());
+
+    // Expect the dropdown to be closed
+    expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
+  });
+
+  it("renders a CustomSelect with simple choices, open and select a choice", () => {
+    const keyFn = jest.fn((item) => item);
+    const labelFn = jest.fn((item) => item);
+    let value = null;
+    const onChange = jest.fn((val) => {
+      value = val;
+    });
+    const choices = ["Choice 1", "Choice 2", "Choice 3", "Choice 4"];
+
+    const { rerender } = render(
+      <DarkModeProvider
+        value={{ darkMode: false, changeDarkMode: jest.fn(() => false) }}
+      >
+        <CustomSelect
+          choices={choices}
+          keyFn={keyFn}
+          labelFn={labelFn}
+          placeholder="This is a placeholder"
+          onChange={onChange}
+        ></CustomSelect>
+      </DarkModeProvider>
+    );
+
+    const customSelect = screen.getByText("This is a placeholder");
+
+    expect(customSelect).toBeInTheDocument();
+
+    // Expect the dropdown to be closed
+    expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
+
+    // Click on the placeholder
+    act(() => customSelect.closest("div").click());
+
+    // Expect the dropdown to be open
+    expect(screen.getByText("Choice 1")).toBeInTheDocument();
+
+    // Click on a choice
+    act(() => screen.getByText("Choice 1").click());
+
+    // Expect the dropdown to be closed
+    expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
+
+    // Expect the placeholder to be updated
+    expect(screen.getByText("Choice 1")).toBeInTheDocument();
+
+    // Expect the value to be updated
+    expect(value).toEqual("Choice 1");
+
+    // Expect the onChange to be called
+    expect(onChange).toHaveBeenCalledTimes(1);
+
+    // Expect the onChange to be called with the correct value
+    expect(onChange).toHaveBeenCalledWith("Choice 1");
+
+    // Expect the keyFn to be called
+    expect(keyFn).toHaveBeenCalledTimes(8);
+
+    // Expect the keyFn to be called with the correct value
+    expect(keyFn).toHaveBeenCalledWith("Choice 1");
+    expect(keyFn).toHaveBeenCalledWith("Choice 2");
+    expect(keyFn).toHaveBeenCalledWith("Choice 3");
+    expect(keyFn).toHaveBeenCalledWith("Choice 4");
+
+    // Expect the labelFn to be called
+    expect(labelFn).toHaveBeenCalledTimes(9);
+
+    // Expect the labelFn to be called with the correct value
+    expect(labelFn).toHaveBeenCalledWith("Choice 1");
+    expect(labelFn).toHaveBeenCalledWith("Choice 2");
+    expect(labelFn).toHaveBeenCalledWith("Choice 3");
+    expect(labelFn).toHaveBeenCalledWith("Choice 4");
+  });
+
+  it("renders a CustomSelect with complex choices, open and select a choice", () => {
+    const keyFn = jest.fn((item) => item.id);
+    const labelFn = jest.fn((item) => item.name);
+    let value = null;
+    const onChange = jest.fn((val) => {
+      value = val;
+    });
+    const choices = [
+      { id: 1, name: "Choice 1" },
+      { id: 2, name: "Choice 2" },
+      { id: 3, name: "Choice 3" },
+      { id: 4, name: "Choice 4" },
+    ];
+
+    const { rerender } = render(
+      <DarkModeProvider
+        value={{ darkMode: false, changeDarkMode: jest.fn(() => false) }}
+      >
+        <CustomSelect
+          choices={choices}
+          keyFn={keyFn}
+          labelFn={labelFn}
+          placeholder="This is a placeholder"
+          onChange={onChange}
+        ></CustomSelect>
+      </DarkModeProvider>
+    );
+
+    const customSelect = screen.getByText("This is a placeholder");
+
+    expect(customSelect).toBeInTheDocument();
+
+    // Expect the dropdown to be closed
+    expect(screen.queryByText("Choice 1")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
+
+    // Click on the placeholder
+    act(() => customSelect.closest("div").click());
+
+    // Expect the dropdown to be open
+    expect(screen.getByText("Choice 1")).toBeInTheDocument();
+
+    // Click on a choice
+    act(() => screen.getByText("Choice 1").click());
+
+    // Expect the dropdown to be closed
+    expect(screen.queryByText("Choice 2")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 3")).not.toBeInTheDocument();
+    expect(screen.queryByText("Choice 4")).not.toBeInTheDocument();
+
+    // Expect the placeholder to be updated
+    expect(screen.getByText("Choice 1")).toBeInTheDocument();
+
+    // Expect the value to be updated
+    expect(value).toEqual({ id: 1, name: "Choice 1" });
+
+    // Expect the onChange to be called
+    expect(onChange).toHaveBeenCalledTimes(1);
+
+    // Expect the onChange to be called with the correct value
+    expect(onChange).toHaveBeenCalledWith({ id: 1, name: "Choice 1" });
+
+    // Expect the keyFn to be called
+    expect(keyFn).toHaveBeenCalledTimes(8);
+
+    // Expect the keyFn to be called with the correct value
+    expect(keyFn).toHaveBeenCalledWith({ id: 1, name: "Choice 1" });
+    expect(keyFn).toHaveBeenCalledWith({ id: 2, name: "Choice 2" });
+    expect(keyFn).toHaveBeenCalledWith({ id: 3, name: "Choice 3" });
+    expect(keyFn).toHaveBeenCalledWith({ id: 4, name: "Choice 4" });
+
+    // Expect the labelFn to be called
+    expect(labelFn).toHaveBeenCalledTimes(9);
+
+    // Expect the labelFn to be called with the correct value
+    expect(labelFn).toHaveBeenCalledWith({ id: 1, name: "Choice 1" });
+    expect(labelFn).toHaveBeenCalledWith({ id: 2, name: "Choice 2" });
+    expect(labelFn).toHaveBeenCalledWith({ id: 3, name: "Choice 3" });
+    expect(labelFn).toHaveBeenCalledWith({ id: 4, name: "Choice 4" });
+  });
 });
