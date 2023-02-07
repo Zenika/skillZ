44b0d61f525a385aeef8bbd9f91bec5b4721fb3f
Benjamin Bourgeois <benjamin.bourgeois@zenika.com>
2023-02-07 15:02:42 +0100

    :rocket: automatically update agencies, categories, certifications, tags and topics referential files (#500)
    * :construction: add script and endpoint
    
    * :rocket: force push at the end
    
    * :rocket: improve workflow
    
    * :rocket: all script to update referentials

---
+++ scripts/update-categories-referentiel.mjs
@@ -0,0 +1,54 @@
+import fetch from 'node-fetch';
+import fs from "fs";
+
+/*
+ * ENVIRONMENT CHECK
+ */
+if (!process.env.NEXT_PUBLIC_BASE_URL) {
+    throw new Error(
+        "ERROR: App couldn't start because NEXT_PUBLIC_BASE_URL isn't defined"
+    );
+}
+
+if (!process.env.NEXT_API_BEARER_TOKEN) {
+    throw new Error(
+        "ERROR: App couldn't start because NEXT_API_BEARER_TOKEN isn't defined"
+    );
+}
+
+/*
+ * GET ALL CATEGORIES FROM API
+ */
+const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/categories`,
+    {
+        method: "GET",
+        headers: {
+            Authorization: process.env.NEXT_API_BEARER_TOKEN,
+        },
+    }
+);
+
+const {categories: categoriesData} = await response.json()
+
+/*
+ * FORMAT DATA
+ */
+const categories = categoriesData.map((cat) => `('${cat.id}', '${cat.label}', ${cat.x}, '${cat.y}', '${cat.color}', ${cat.index})`)
+
+/*
+ * WRITE DATA TO FILE
+ */
+let writer = fs.createWriteStream('../hasura/seeds/02-categories.sql', {
+    flags: 'w'
+});
+
+writer.write("INSERT INTO public.\"Category\" VALUES\n")
+
+writer.write(categories.join(",\n"))
+
+writer.write("\n ON CONFLICT (\"id\") DO UPDATE SET \"label\" = EXCLUDED.\"label\", \"color\" = EXCLUDED.\"color\";")
+
+writer.close()
+
+console.log(`Referential Categories successfully updated (${categories.length} categories).`)
---
+++ scripts/update-topics-referentiel.mjs
@@ -0,0 +1,59 @@
+import fetch from 'node-fetch';
+import fs from "fs";
+
+/*
+ * ENVIRONMENT CHECK
+ */
+if (!process.env.NEXT_PUBLIC_BASE_URL) {
+    throw new Error(
+        "ERROR: App couldn't start because NEXT_PUBLIC_BASE_URL isn't defined"
+    );
+}
+
+if (!process.env.NEXT_API_BEARER_TOKEN) {
+    throw new Error(
+        "ERROR: App couldn't start because NEXT_API_BEARER_TOKEN isn't defined"
+    );
+}
+
+/*
+ * GET ALL TOPICS FROM API
+ */
+const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/topics`,
+    {
+        method: "GET",
+        headers: {
+            Authorization: process.env.NEXT_API_BEARER_TOKEN,
+        },
+    }
+);
+
+const {topics: topicsData} = await response.json()
+
+/*
+ * FORMAT DATA
+ */
+const topics = topicsData.map((topic) => `('${topic.type.replaceAll("'", "''")}', '${topic.name.replaceAll("'", "''")}')`)
+
+/*
+ * WRITE DATA TO FILE
+ */
+let writer = fs.createWriteStream('../hasura/seeds/04-topics.sql', {
+    flags: 'w'
+});
+
+writer.write("INSERT INTO \"public\".\"Topic\" (\"type\", \"name\") VALUES\n")
+
+writer.write(topics.join(",\n"))
+
+writer.write("\n ON CONFLICT (\"name\") DO UPDATE SET \"type\" = EXCLUDED.\"type\";")
+
+writer.write("\n\n")
+
+writer.write("DELETE FROM \"public\".\"UserTopic\" WHERE \"topicId\" IN (SELECT \"id\" FROM \"public\".\"Topic\" WHERE \"type\" IS NULL);\n")
+writer.write("DELETE FROM \"public\".\"Topic\" WHERE \"type\" IS NULL;\n")
+
+writer.close()
+
+console.log(`Referential Topics successfully updated (${topics.length} topics).`)
