870faef66cd0533493c04c4a8156bafb17596110
Benjamin Bourgeois <benjishift@gmail.com>
2023-02-22 17:43:16 +0100

    :rocket: add click only on icon in certification panel

--- src/components/organisms/CertificationPanel.tsx
+++ src/components/organisms/CertificationPanel.tsx
@@ -19,19 +19,21 @@ const CertificationPanel = ({
     <>
       <div
         className={`flex flex-row bg-light-light dark:bg-dark-light px-4 py-4 mx-2 my-1 rounded-lg items-center border border-light-light dark:border-dark-light`}
-        onClick={(e) => {
-          e.stopPropagation();
-          push(linkToCertification);
-        }}
       >
         <div className={`flex flex-col w-full`}>
           <div className="flex flex-row justify-between">
             <div className="flex flex-col">
-              <h2 className="text-xl cursor-pointer">{name}</h2>
+              <h2 className="text-xl">{name}</h2>
             </div>
           </div>
         </div>
-        <div className="flex flex-row items-center justify-around rounded-full w-16 px-1 py-1 bg-light-med dark:bg-dark-med h-8">
+        <div
+          className="flex flex-row items-center justify-around rounded-full w-16 px-1 py-1 bg-light-med dark:bg-dark-med h-8 cursor-pointer hover:bg-light-dark hover:border-light-graybutton hover:dark:bg-dark-radargrid hover:dark:border-dark-graybutton"
+          onClick={(e) => {
+            e.stopPropagation();
+            push(linkToCertification);
+          }}
+        >
           <span>
             {certification.UserCertifications_aggregate.aggregate.count}
           </span>
