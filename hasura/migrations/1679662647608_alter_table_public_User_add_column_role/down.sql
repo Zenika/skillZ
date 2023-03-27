f18bde1ba0cb8d2151405c40603d34e94097cb72
bbourgeois <benjamin.bourgeois@zenika.com>
2023-03-27 11:29:14 +0200

    :card_file_box: add down migration

--- hasura/migrations/1679662647608_alter_table_public_User_add_column_role/down.sql
+++ hasura/migrations/1679662647608_alter_table_public_User_add_column_role/down.sql
@@ -2,3 +2,4 @@
 -- Please write an appropriate down migration for the SQL below:
 -- alter table "public"."User" add column "role" Text
 --  not null default 'world';
+alter table "public"."User" drop column if exists "role";
