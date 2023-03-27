a260b442aea04534321865ba72bb0b97a01ff737
bbourgeois <benjamin.bourgeois@zenika.com>
2023-03-27 11:19:07 +0200

    :sparkles: add admin endpoint and roles

---
+++ src/utils/fetchers/getAllUsers.tsx
@@ -0,0 +1,27 @@
+import { of } from "await-of";
+import { fetcher } from "../fetcher";
+
+export const GetAllUsersQuery = `
+query getAllUsers($whereConditions: User_bool_exp!) {
+  User(where: $whereConditions) {
+    role
+    name
+    email
+    active
+  }
+}`;
+
+export const GetAllUsersFetcher = async (roles: String[]) => {
+  const whereConditions = roles.length ? { role: { _in: roles } } : {};
+
+  const [response, err] = await of(
+    fetcher(GetAllUsersQuery, { whereConditions })
+  );
+
+  if (err) {
+    console.error(err);
+    return null;
+  }
+
+  return await response.json();
+};
