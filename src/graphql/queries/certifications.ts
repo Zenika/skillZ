0465a62fe13682c2d6e88aa30dc460c9cb6f5abd
Adrien Nortain <43404315+ZenikAdrien@users.noreply.github.com>
2024-10-28 09:28:35 +0100

    Feat(user): only display for active users (#721)

--- src/graphql/queries/certifications.ts
+++ src/graphql/queries/certifications.ts
@@ -2,7 +2,12 @@ import { gql } from "@apollo/client";
 
 export const GET_USER_CERTIFICATION = gql`
   query getUserCertification($name: String!) {
-    UserCertification(where: { Certification: { name: { _eq: $name } } }) {
+    UserCertification(
+      where: {
+        Certification: { name: { _eq: $name } }
+        User: { active: { _eq: true } }
+      }
+    ) {
       userEmail
       certId
       from
