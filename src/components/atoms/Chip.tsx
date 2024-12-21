2efadec6f62496f32fe7a6edb889352216b8da87
bbourgeois <me@bbourgeois.dev>
2024-12-21 20:31:06 +0100

    :arrow_up: upgrade to next 15

--- src/components/atoms/Chip.tsx
+++ src/components/atoms/Chip.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { type JSX } from "react";
 import { IoIosCloseCircle } from "react-icons/io";
 
 export type ChipType = "primary" | "secondary" | "tertiary";
